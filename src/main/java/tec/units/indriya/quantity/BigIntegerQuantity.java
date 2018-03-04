cc54041c609e474f1937edcf671b2d5ed618ff1b
Werner Keil <werner.keil@gmx.net>
2018-03-04 16:19:33 +0100

    31: Add BigIntegerQuantity
    Task-Url: https://github.com/unitsofmeasurement/indriya/issues/issues/31

---
+++ src/main/java/tec/units/indriya/quantity/BigIntegerQuantity.java
@@ -0,0 +1,172 @@
+/*
+ * Units of Measurement Reference Implementation
+ * Copyright (c) 2005-2018, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-385, Indriya nor the names of their contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.units.indriya.quantity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.Objects;
+
+import javax.measure.Quantity;
+import javax.measure.Unit;
+
+import tec.units.indriya.AbstractConverter;
+import tec.units.indriya.AbstractQuantity;
+import tec.units.indriya.ComparableQuantity;
+
+/**
+ * An amount of quantity, implementation of {@link ComparableQuantity} that uses {@link BigInteger} as implementation of {@link Number}, this object
+ * is immutable. Note: all operations which involves {@link Number}, this implementation will convert to {@link BigInteger}.
+ *
+ * @param <Q>
+ *          The type of the quantity.
+ * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
+ * @see AbstractQuantity
+ * @see Quantity
+ * @see ComparableQuantity
+ * @version 0.1
+ * @since 2.0
+ */
+@SuppressWarnings({ "rawtypes", "unchecked" })
+final class BigIntegerQuantity<Q extends Quantity<Q>> extends AbstractQuantity<Q> implements Serializable {
+
+  /**
+   * 
+   */
+private static final long serialVersionUID = -593014349777834846L;
+private final BigInteger value;
+
+  public BigIntegerQuantity(BigInteger value, Unit<Q> unit) {
+    super(unit);
+    this.value = value;
+  }
+
+  @Override
+  public BigInteger getValue() {
+    return value;
+  }
+
+  @Override
+  public double doubleValue(Unit<Q> unit) {
+    return (unit.equals(unit)) ? value.doubleValue() : unit.getConverterTo(unit).convert(value.doubleValue());
+  }
+
+  @Override
+  public BigDecimal decimalValue(Unit<Q> unit, MathContext ctx) throws ArithmeticException {
+    if (super.getUnit().equals(unit)) {
+    	return  new BigDecimal(value);
+    } else {
+    	final Number converted = ((AbstractConverter) unit.getConverterTo(unit)).convert(value);
+    	if (converted instanceof BigDecimal) {
+    		return BigDecimal.class.cast(converted);
+    	} else if (converted instanceof BigInteger) {
+    		return new BigDecimal(BigInteger.class.cast(converted));
+    	} else {
+    		return BigDecimal.valueOf(converted.doubleValue());
+    	}
+    }
+  }
+
+  @Override
+  public ComparableQuantity<Q> add(Quantity<Q> that) {
+    if (getUnit().equals(that.getUnit())) {
+      return Quantities.getQuantity(value.add(Equalizer.toBigInteger(that.getValue())), getUnit());
+    }
+    Quantity<Q> converted = that.to(getUnit());
+    return Quantities.getQuantity(value.add(Equalizer.toBigInteger(converted.getValue())), getUnit());
+  }
+
+  @Override
+  public ComparableQuantity<Q> subtract(Quantity<Q> that) {
+    if (getUnit().equals(that.getUnit())) {
+      return Quantities.getQuantity(value.subtract(Equalizer.toBigInteger(that.getValue())), getUnit());
+    }
+    Quantity<Q> converted = that.to(getUnit());
+    return Quantities.getQuantity(value.subtract(Equalizer.toBigInteger(converted.getValue())), getUnit());
+  }
+
+  @Override
+  public ComparableQuantity<?> multiply(Quantity<?> that) {
+    return new BigIntegerQuantity(value.multiply(Equalizer.toBigDecimal(that.getValue()).toBigInteger()), getUnit().multiply(that.getUnit()));
+  }
+
+  @Override
+  public ComparableQuantity<Q> multiply(Number that) {
+    return Quantities.getQuantity(value.multiply(Equalizer.toBigInteger(that)), getUnit());
+  }
+
+  @Override
+  public ComparableQuantity<Q> divide(Number that) {
+    return Quantities.getQuantity(value.divide(Equalizer.toBigDecimal(that).toBigInteger()), getUnit());
+  }
+
+  @Override
+  public ComparableQuantity<Q> inverse() {
+    return (ComparableQuantity<Q>) Quantities.getQuantity(BigInteger.ONE.divide(value), getUnit().inverse());
+  }
+
+  @Override
+  protected long longValue(Unit<Q> unit) {
+    double result = doubleValue(unit);
+    if ((result < Long.MIN_VALUE) || (result > Long.MAX_VALUE)) {
+      throw new ArithmeticException("Overflow (" + result + ")");
+    }
+    return (long) result;
+  }
+
+  @Override
+  public boolean isBig() {
+    return true;
+  }
+
+  @Override
+  public ComparableQuantity<?> divide(Quantity<?> that) {
+    return new BigIntegerQuantity(value.divide(Equalizer.toBigInteger(that.getValue())), getUnit().divide(that.getUnit()));
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see AbstractQuantity#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null)
+      return false;
+    if (obj == this)
+      return true;
+    if (obj instanceof Quantity<?>) {
+      Quantity<?> that = (Quantity<?>) obj;
+      return Objects.equals(getUnit(), that.getUnit()) && Equalizer.hasEquality(value, that.getValue());
+    }
+    return false;
+  }
+}
---
+++ src/test/java/tec/units/indriya/quantity/BigIntegerQuantityTest.java
@@ -0,0 +1,136 @@
+/*
+ * Units of Measurement Reference Implementation
+ * Copyright (c) 2005-2018, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-385, Indriya nor the names of their contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.units.indriya.quantity;
+
+import static org.junit.Assert.assertEquals;
+import javax.measure.Quantity;
+import javax.measure.quantity.ElectricResistance;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Time;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import tec.units.indriya.quantity.IntegerQuantity;
+import tec.units.indriya.quantity.Quantities;
+import tec.units.indriya.unit.MetricPrefix;
+import tec.units.indriya.unit.Units;
+
+public class BigIntegerQuantityTest {
+
+  @Test
+  public void divideTest() {
+    IntegerQuantity<ElectricResistance> quantity1 = new IntegerQuantity<ElectricResistance>(Long.valueOf(3).intValue(), Units.OHM);
+    IntegerQuantity<ElectricResistance> quantity2 = new IntegerQuantity<ElectricResistance>(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<?> result = quantity1.divide(quantity2);
+    assertEquals(Double.valueOf(1.5d), result.getValue());
+  }
+
+  @SuppressWarnings({ "unchecked", "rawtypes" })
+  @Test
+  public void addTest() {
+    IntegerQuantity quantity1 = new IntegerQuantity(Long.valueOf(1).intValue(), Units.OHM);
+    IntegerQuantity quantity2 = new IntegerQuantity(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<ElectricResistance> result = quantity1.add(quantity2);
+    assertEquals(Short.valueOf("3").intValue(), result.getValue().intValue());
+  }
+
+  @Test
+  public void subtractTest() {
+    IntegerQuantity<ElectricResistance> quantity1 = new IntegerQuantity<ElectricResistance>(Long.valueOf(1).intValue(), Units.OHM);
+    IntegerQuantity<ElectricResistance> quantity2 = new IntegerQuantity<ElectricResistance>(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<ElectricResistance> result = quantity2.subtract(quantity1);
+    assertEquals(Short.valueOf("1").intValue(), result.getValue().intValue());
+    assertEquals(Units.OHM, result.getUnit());
+  }
+
+  @Test
+  public void multiplyQuantityTest() {
+    IntegerQuantity<ElectricResistance> quantity1 = new IntegerQuantity<ElectricResistance>(Long.valueOf(3).intValue(), Units.OHM);
+    IntegerQuantity<ElectricResistance> quantity2 = new IntegerQuantity<ElectricResistance>(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<?> result = quantity1.multiply(quantity2);
+    assertEquals(Integer.valueOf(6), result.getValue());
+  }
+
+  @Test
+  public void longValueTest() {
+    IntegerQuantity<Time> day = new IntegerQuantity<Time>(Integer.valueOf(3).intValue(), Units.DAY);
+    long hours = day.longValue(Units.HOUR);
+    assertEquals(72L, hours);
+  }
+
+  @Test
+  public void doubleValueTest() {
+    IntegerQuantity<Time> day = new IntegerQuantity<Time>(Integer.valueOf(3).intValue(), Units.DAY);
+    double hours = day.doubleValue(Units.HOUR);
+    assertEquals(72D, hours, 0);
+  }
+
+  @Test
+  public void toTest() {
+    Quantity<Time> day = Quantities.getQuantity(1D, Units.DAY);
+    Quantity<Time> hour = day.to(Units.HOUR);
+    Assert.assertEquals(hour.getValue().intValue(), 24);
+    Assert.assertEquals(hour.getUnit(), Units.HOUR);
+
+    Quantity<Time> dayResult = hour.to(Units.DAY);
+    Assert.assertEquals(dayResult.getValue().intValue(), day.getValue().intValue());
+    Assert.assertEquals(dayResult.getValue().intValue(), day.getValue().intValue());
+  }
+
+  @Test
+  public void testEquality() throws Exception {
+    Quantity<Length> value = Quantities.getQuantity(new Integer(10), Units.METRE);
+    Quantity<Length> anotherValue = Quantities.getQuantity(new Integer(10), Units.METRE);
+    assertEquals(value, anotherValue);
+  }
+  
+  @Test
+  public void milliOhmTest() {
+	  final IntegerQuantity<ElectricResistance> ONE_OHM = new IntegerQuantity<ElectricResistance>(Integer.valueOf(1).intValue(), Units.OHM);
+	  final IntegerQuantity<ElectricResistance> ONE_MILLIOHM = new IntegerQuantity<ElectricResistance>(Integer.valueOf(1).intValue(), MetricPrefix.MILLI(Units.OHM));
+
+	  assertEquals(ONE_OHM, ONE_OHM.add(ONE_MILLIOHM));
+	  final IntegerQuantity<ElectricResistance> ONEOONE_MILLIOHM = new IntegerQuantity<ElectricResistance>(Integer.valueOf(1001), MetricPrefix.MILLI(Units.OHM));
+	  assertEquals(ONEOONE_MILLIOHM, ONE_MILLIOHM.add(ONE_OHM));
+  }
+  
+  @Test
+  public void yottaOhmTest() {
+	  final IntegerQuantity<ElectricResistance> ONE_OHM = new IntegerQuantity<ElectricResistance>(Integer.valueOf(1).intValue(), Units.OHM);
+	  final IntegerQuantity<ElectricResistance> ONE_YOTTAOHM = new IntegerQuantity<ElectricResistance>(Integer.valueOf(1).intValue(), MetricPrefix.YOTTA(Units.OHM));
+	  final IntegerQuantity<ElectricResistance> INT_MIN_OHM = new IntegerQuantity<ElectricResistance>(Integer.MIN_VALUE, Units.OHM);
+
+	  assertEquals(INT_MIN_OHM, ONE_OHM.add(ONE_YOTTAOHM));
+	  final IntegerQuantity<ElectricResistance> ONEOONE_MILLIOHM = new IntegerQuantity<ElectricResistance>(Integer.valueOf(1001), MetricPrefix.MILLI(Units.OHM));
+	  //assertEquals(ONEOONE_MILLIOHM, ONE_MILLIOHM.add(ONE_OHM));
+  }
+  
+}
