562f1a420dc44b70f2a8dc511e1fe81bf88bc116
Filip van Laenen <f.a.vanlaenen@ieee.org>
2018-07-05 09:07:38 +0200

    Reformatted DefaultQuantityFactory, plus changed getInstance to return same factory instance when called with the same class

--- src/main/java/tech/units/indriya/quantity/DefaultQuantityFactory.java
+++ src/main/java/tech/units/indriya/quantity/DefaultQuantityFactory.java
@@ -109,8 +109,7 @@ import javax.measure.spi.QuantityFactory;
 import tech.units.indriya.AbstractUnit;
 
 /**
- * A factory producing simple quantities instances (tuples {@link Number}/
- * {@link Unit}).<br>
+ * A factory producing simple quantities instances (tuples {@link Number}/ {@link Unit}).<br>
  *
  * For example:<br>
  * <code>
@@ -119,10 +118,9 @@ import tech.units.indriya.AbstractUnit;
  * </code>
  * 
  * @param <Q>
- *            The type of the quantity.
+ *          The type of the quantity.
  *
- * @author <a href="mailto:martin.desruisseaux@geomatys.com">Martin
- *         Desruisseaux</a>
+ * @author <a href="mailto:martin.desruisseaux@geomatys.com">Martin Desruisseaux</a>
  * @author <a href="mailto:units@catmedia.us">Werner Keil</a>
  * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @author <a href="mailto:otaviojava@java.net">Otavio Santana</a>
@@ -130,122 +128,119 @@ import tech.units.indriya.AbstractUnit;
  * @since 1.0
  */
 public class DefaultQuantityFactory<Q extends Quantity<Q>> implements QuantityFactory<Q> {
-	@SuppressWarnings("rawtypes")
-	static final Map<Class, QuantityFactory> INSTANCES = new HashMap<>();
+  @SuppressWarnings("rawtypes")
+  static final Map<Class, QuantityFactory> INSTANCES = new HashMap<>();
 
-	static final Logger logger = Logger.getLogger(DefaultQuantityFactory.class.getName());
+  static final Logger logger = Logger.getLogger(DefaultQuantityFactory.class.getName());
 
-	static final Level LOG_LEVEL = Level.FINE;
+  static final Level LOG_LEVEL = Level.FINE;
 
-	/**
-	 * The type of the quantities created by this factory.
-	 */
-	private final Class<Q> type;
+  /**
+   * The type of the quantities created by this factory.
+   */
+  private final Class<Q> type;
 
-	/**
-	 * The system unit for quantities created by this factory.
-	 */
-	private final Unit<Q> systemUnit;
+  /**
+   * The system unit for quantities created by this factory.
+   */
+  private final Unit<Q> systemUnit;
 
-	@SuppressWarnings("rawtypes")
-	private static final Map<Class, Unit> CLASS_TO_SYSTEM_UNIT = new ConcurrentHashMap<>();
+  @SuppressWarnings("rawtypes")
+  private static final Map<Class, Unit> CLASS_TO_SYSTEM_UNIT = new ConcurrentHashMap<>();
 
-	static {
-		CLASS_TO_SYSTEM_UNIT.put(Dimensionless.class, AbstractUnit.ONE);
-		CLASS_TO_SYSTEM_UNIT.put(ElectricCurrent.class, AMPERE);
-		CLASS_TO_SYSTEM_UNIT.put(LuminousIntensity.class, CANDELA);
-		CLASS_TO_SYSTEM_UNIT.put(Temperature.class, KELVIN);
-		CLASS_TO_SYSTEM_UNIT.put(Mass.class, KILOGRAM);
-		CLASS_TO_SYSTEM_UNIT.put(Length.class, METRE);
-		CLASS_TO_SYSTEM_UNIT.put(AmountOfSubstance.class, MOLE);
-		CLASS_TO_SYSTEM_UNIT.put(Time.class, SECOND);
-		CLASS_TO_SYSTEM_UNIT.put(Angle.class, RADIAN);
-		CLASS_TO_SYSTEM_UNIT.put(SolidAngle.class, STERADIAN);
-		CLASS_TO_SYSTEM_UNIT.put(Frequency.class, HERTZ);
-		CLASS_TO_SYSTEM_UNIT.put(Force.class, NEWTON);
-		CLASS_TO_SYSTEM_UNIT.put(Pressure.class, PASCAL);
-		CLASS_TO_SYSTEM_UNIT.put(Energy.class, JOULE);
-		CLASS_TO_SYSTEM_UNIT.put(Power.class, WATT);
-		CLASS_TO_SYSTEM_UNIT.put(ElectricCharge.class, COULOMB);
-		CLASS_TO_SYSTEM_UNIT.put(ElectricPotential.class, VOLT);
-		CLASS_TO_SYSTEM_UNIT.put(ElectricCapacitance.class, FARAD);
-		CLASS_TO_SYSTEM_UNIT.put(ElectricResistance.class, OHM);
-		CLASS_TO_SYSTEM_UNIT.put(ElectricConductance.class, SIEMENS);
-		CLASS_TO_SYSTEM_UNIT.put(MagneticFlux.class, WEBER);
-		CLASS_TO_SYSTEM_UNIT.put(MagneticFluxDensity.class, TESLA);
-		CLASS_TO_SYSTEM_UNIT.put(ElectricInductance.class, HENRY);
-		CLASS_TO_SYSTEM_UNIT.put(LuminousFlux.class, LUMEN);
-		CLASS_TO_SYSTEM_UNIT.put(Illuminance.class, LUX);
-		CLASS_TO_SYSTEM_UNIT.put(Radioactivity.class, BECQUEREL);
-		CLASS_TO_SYSTEM_UNIT.put(RadiationDoseAbsorbed.class, GRAY);
-		CLASS_TO_SYSTEM_UNIT.put(RadiationDoseEffective.class, SIEVERT);
-		CLASS_TO_SYSTEM_UNIT.put(CatalyticActivity.class, KATAL);
-		CLASS_TO_SYSTEM_UNIT.put(Speed.class, METRE_PER_SECOND);
-		CLASS_TO_SYSTEM_UNIT.put(Acceleration.class, METRE_PER_SQUARE_SECOND);
-		CLASS_TO_SYSTEM_UNIT.put(Area.class, SQUARE_METRE);
-		CLASS_TO_SYSTEM_UNIT.put(Volume.class, CUBIC_METRE);
-	}
+  static {
+    CLASS_TO_SYSTEM_UNIT.put(Dimensionless.class, AbstractUnit.ONE);
+    CLASS_TO_SYSTEM_UNIT.put(ElectricCurrent.class, AMPERE);
+    CLASS_TO_SYSTEM_UNIT.put(LuminousIntensity.class, CANDELA);
+    CLASS_TO_SYSTEM_UNIT.put(Temperature.class, KELVIN);
+    CLASS_TO_SYSTEM_UNIT.put(Mass.class, KILOGRAM);
+    CLASS_TO_SYSTEM_UNIT.put(Length.class, METRE);
+    CLASS_TO_SYSTEM_UNIT.put(AmountOfSubstance.class, MOLE);
+    CLASS_TO_SYSTEM_UNIT.put(Time.class, SECOND);
+    CLASS_TO_SYSTEM_UNIT.put(Angle.class, RADIAN);
+    CLASS_TO_SYSTEM_UNIT.put(SolidAngle.class, STERADIAN);
+    CLASS_TO_SYSTEM_UNIT.put(Frequency.class, HERTZ);
+    CLASS_TO_SYSTEM_UNIT.put(Force.class, NEWTON);
+    CLASS_TO_SYSTEM_UNIT.put(Pressure.class, PASCAL);
+    CLASS_TO_SYSTEM_UNIT.put(Energy.class, JOULE);
+    CLASS_TO_SYSTEM_UNIT.put(Power.class, WATT);
+    CLASS_TO_SYSTEM_UNIT.put(ElectricCharge.class, COULOMB);
+    CLASS_TO_SYSTEM_UNIT.put(ElectricPotential.class, VOLT);
+    CLASS_TO_SYSTEM_UNIT.put(ElectricCapacitance.class, FARAD);
+    CLASS_TO_SYSTEM_UNIT.put(ElectricResistance.class, OHM);
+    CLASS_TO_SYSTEM_UNIT.put(ElectricConductance.class, SIEMENS);
+    CLASS_TO_SYSTEM_UNIT.put(MagneticFlux.class, WEBER);
+    CLASS_TO_SYSTEM_UNIT.put(MagneticFluxDensity.class, TESLA);
+    CLASS_TO_SYSTEM_UNIT.put(ElectricInductance.class, HENRY);
+    CLASS_TO_SYSTEM_UNIT.put(LuminousFlux.class, LUMEN);
+    CLASS_TO_SYSTEM_UNIT.put(Illuminance.class, LUX);
+    CLASS_TO_SYSTEM_UNIT.put(Radioactivity.class, BECQUEREL);
+    CLASS_TO_SYSTEM_UNIT.put(RadiationDoseAbsorbed.class, GRAY);
+    CLASS_TO_SYSTEM_UNIT.put(RadiationDoseEffective.class, SIEVERT);
+    CLASS_TO_SYSTEM_UNIT.put(CatalyticActivity.class, KATAL);
+    CLASS_TO_SYSTEM_UNIT.put(Speed.class, METRE_PER_SECOND);
+    CLASS_TO_SYSTEM_UNIT.put(Acceleration.class, METRE_PER_SQUARE_SECOND);
+    CLASS_TO_SYSTEM_UNIT.put(Area.class, SQUARE_METRE);
+    CLASS_TO_SYSTEM_UNIT.put(Volume.class, CUBIC_METRE);
+  }
 
-	@SuppressWarnings("unchecked")
-	private DefaultQuantityFactory(Class<Q> quantity) {
-		type = quantity;
-		systemUnit = CLASS_TO_SYSTEM_UNIT.get(type);
-	}
+  @SuppressWarnings("unchecked")
+  private DefaultQuantityFactory(Class<Q> quantity) {
+    type = quantity;
+    systemUnit = CLASS_TO_SYSTEM_UNIT.get(type);
+  }
 
-	/**
-	 * Returns the default instance for the specified quantity type.
-	 *
-	 * @param <Q>
-	 *            The type of the quantity
-	 * @param type
-	 *            the quantity type
-	 * @return the quantity factory for the specified type
-	 */
-	@SuppressWarnings("unchecked")
-	public static <Q extends Quantity<Q>> QuantityFactory<Q> getInstance(final Class<Q> type) {
-		logger.log(LOG_LEVEL, "Type: " + type + ": " + type.isInterface());
-		QuantityFactory<Q> factory;
-		if (!type.isInterface()) {
-			factory = new DefaultQuantityFactory<Q>(type);
-			// TODO use instances?
-		} else {
-			factory = INSTANCES.get(type);
-			if (factory != null)
-				return factory;
-			if (!Quantity.class.isAssignableFrom(type))
-				// This exception is not documented because it should never
-				// happen if the
-				// user don't try to trick the Java generic types system with
-				// unsafe cast.
-				throw new ClassCastException();
-			factory = new DefaultQuantityFactory<Q>(type);
-			INSTANCES.put(type, factory);
-		}
-		return factory;
-	}
+  /**
+   * Returns the default instance for the specified quantity type.
+   *
+   * @param <Q>
+   *          The type of the quantity
+   * @param type
+   *          the quantity type
+   * @return the quantity factory for the specified type
+   */
+  @SuppressWarnings("unchecked")
+  public static <Q extends Quantity<Q>> QuantityFactory<Q> getInstance(final Class<Q> type) {
+    logger.log(LOG_LEVEL, "Type: " + type + ": " + type.isInterface());
+    QuantityFactory<Q> factory;
+    factory = INSTANCES.get(type);
+    if (factory != null) {
+      return factory;
+    }
+    if (!Quantity.class.isAssignableFrom(type)) {
+      // This exception is not documented because it should never
+      // happen if the
+      // user don't try to trick the Java generic types system with
+      // unsafe cast.
+      throw new ClassCastException();
+    }
+    factory = new DefaultQuantityFactory<Q>(type);
+    INSTANCES.put(type, factory);
+    return factory;
+  }
 
-	public String toString() {
-		return "tech.units.indriya.DefaultQuantityFactory <" + type.getName() + '>';
-	}
+  public String toString() {
+    return "tech.units.indriya.DefaultQuantityFactory <" + type.getName() + '>';
+  }
 
-	public boolean equals(Object obj) {
-		if (DefaultQuantityFactory.class.isInstance(obj)) {
-			@SuppressWarnings("rawtypes")
-			DefaultQuantityFactory other = DefaultQuantityFactory.class.cast(obj);
-			return Objects.equals(type, other.type);
-		}
-		return false;
-	}
+  public boolean equals(Object obj) {
+    if (DefaultQuantityFactory.class.isInstance(obj)) {
+      @SuppressWarnings("rawtypes")
+      DefaultQuantityFactory other = DefaultQuantityFactory.class.cast(obj);
+      return Objects.equals(type, other.type);
+    }
+    return false;
+  }
 
-	public int hashCode() {
-		return type.hashCode();
-	}
+  public int hashCode() {
+    return type.hashCode();
+  }
 
-	public Quantity<Q> create(Number value, Unit<Q> unit) {
-		return Quantities.getQuantity(value, unit);
-	}
+  public Quantity<Q> create(Number value, Unit<Q> unit) {
+    return Quantities.getQuantity(value, unit);
+  }
 
-	public Unit<Q> getSystemUnit() {
-		return systemUnit;
-	}
+  public Unit<Q> getSystemUnit() {
+    return systemUnit;
+  }
 }
