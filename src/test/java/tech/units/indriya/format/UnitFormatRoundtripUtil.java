b8d94450b5b08f515bd1edc236a5b502e5a6ae4b
Werner Keil <werner.keil@gmx.net>
2019-01-01 02:33:34 +0100

    175: Update currentYear in pom.xml
    Task-Url: https://github.com/unitsofmeasurement/indriya/issues/issues/175

--- src/test/java/tech/units/indriya/format/UnitFormatRoundtripUtil.java
+++ src/test/java/tech/units/indriya/format/UnitFormatRoundtripUtil.java
@@ -1,6 +1,6 @@
 /*
  * Units of Measurement Reference Implementation
- * Copyright (c) 2005-2018, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ * Copyright (c) 2005-2019, Units of Measurement project.
  *
  * All rights reserved.
  *
