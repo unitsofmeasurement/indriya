24b081c2d7f7c7c169959d1daf20f08f50f0583f
Werner Keil <werner.keil@gmx.net>
2019-06-28 22:19:38 +0200

    196: Rational Prefix?
    Task-Url: https://github.com/unitsofmeasurement/unit-api/issues/issues/196

--- src/test/java/tech/units/indriya/unit/PrefixTest.java
+++ src/test/java/tech/units/indriya/unit/PrefixTest.java
@@ -77,225 +77,224 @@ import tech.units.indriya.function.RationalNumber;
 import tech.units.indriya.quantity.Quantities;
 
 public class PrefixTest {
-  @Test
-  public void testKilo() {
-    // TODO how to handle equals for units?
-    // assertEquals(KILOGRAM.getSymbol(), KILO(GRAM).getSymbol());
-    assertEquals(KILOGRAM.toString(), KILO(GRAM).toString());
-  }
+	@Test
+	public void testKilo() {
+		// TODO how to handle equals for units?
+		// assertEquals(KILOGRAM.getSymbol(), KILO(GRAM).getSymbol());
+		assertEquals(KILOGRAM.toString(), KILO(GRAM).toString());
+	}
 
-  @Test
-  public void testMega() {
-    Quantity<Mass> m1 = Quantities.getQuantity(1.0, MEGA(Units.GRAM));
-    assertEquals(1d, m1.getValue());
-    assertEquals("Mg", m1.getUnit().toString());
-  }
+	@Test
+	public void testMega() {
+		Quantity<Mass> m1 = Quantities.getQuantity(1.0, MEGA(Units.GRAM));
+		assertEquals(1d, m1.getValue());
+		assertEquals("Mg", m1.getUnit().toString());
+	}
 
-  @Test
-  public void testDeci() {
-    Quantity<Volume> m1 = Quantities.getQuantity(1.0, LITRE);
-    assertEquals(1d, m1.getValue());
-    assertEquals("l", m1.getUnit().toString());
+	@Test
+	public void testDeci() {
+		Quantity<Volume> m1 = Quantities.getQuantity(1.0, LITRE);
+		assertEquals(1d, m1.getValue());
+		assertEquals("l", m1.getUnit().toString());
 
-    Quantity<Volume> m2 = m1.to(DECI(LITRE));
-    assertNumberEquals(10, m2.getValue(), 1E-12);
-    assertEquals("dl", m2.getUnit().toString());
-  }
+		Quantity<Volume> m2 = m1.to(DECI(LITRE));
+		assertNumberEquals(10, m2.getValue(), 1E-12);
+		assertEquals("dl", m2.getUnit().toString());
+	}
 
-  @Test
-  public void testMilli() {
-    Quantity<Mass> m1 = Quantities.getQuantity(1.0, MILLI(Units.GRAM));
-    assertEquals(1d, m1.getValue());
-    assertEquals("mg", m1.getUnit().toString());
-  }
+	@Test
+	public void testMilli() {
+		Quantity<Mass> m1 = Quantities.getQuantity(1.0, MILLI(Units.GRAM));
+		assertEquals(1d, m1.getValue());
+		assertEquals("mg", m1.getUnit().toString());
+	}
 
-  @Test
-  public void testMilli2() {
-    Quantity<Volume> m1 = Quantities.getQuantity(10, MILLI(LITRE));
-    assertEquals(10, m1.getValue());
-    assertEquals("ml", m1.getUnit().toString());
-  }
+	@Test
+	public void testMilli2() {
+		Quantity<Volume> m1 = Quantities.getQuantity(10, MILLI(LITRE));
+		assertEquals(10, m1.getValue());
+		assertEquals("ml", m1.getUnit().toString());
+	}
 
-  @Test
-  public void testMilli3() {
-    Quantity<Volume> m1 = Quantities.getQuantity(1.0, LITRE);
-    assertEquals(1d, m1.getValue());
-    assertEquals("l", m1.getUnit().toString());
+	@Test
+	public void testMilli3() {
+		Quantity<Volume> m1 = Quantities.getQuantity(1.0, LITRE);
+		assertEquals(1d, m1.getValue());
+		assertEquals("l", m1.getUnit().toString());
 
-    Quantity<Volume> m2 = m1.to(MILLI(LITRE));
-    assertNumberEquals(1000L, m2.getValue(), 1E-12);
-    assertEquals("ml", m2.getUnit().toString());
-  }
+		Quantity<Volume> m2 = m1.to(MILLI(LITRE));
+		assertNumberEquals(1000L, m2.getValue(), 1E-12);
+		assertEquals("ml", m2.getUnit().toString());
+	}
 
-  @Test
-  public void testMilli4() {
-    Quantity<Volume> m1 = Quantities.getQuantity(1.0, MILLI(LITRE));
-    assertEquals(1d, m1.getValue());
-    assertEquals("ml", m1.getUnit().toString());
+	@Test
+	public void testMilli4() {
+		Quantity<Volume> m1 = Quantities.getQuantity(1.0, MILLI(LITRE));
+		assertEquals(1d, m1.getValue());
+		assertEquals("ml", m1.getUnit().toString());
 
-    Quantity<Volume> m2 = m1.to(LITRE);
-    assertNumberEquals(0.001d, m2.getValue(), 1E-12);
-    assertEquals("l", m2.getUnit().toString());
-  }
+		Quantity<Volume> m2 = m1.to(LITRE);
+		assertNumberEquals(0.001d, m2.getValue(), 1E-12);
+		assertEquals("l", m2.getUnit().toString());
+	}
 
-  @Test
-  public void testMicro2() {
-    Quantity<Length> m1 = Quantities.getQuantity(1.0, Units.METRE);
-    assertEquals(1d, m1.getValue());
-    assertEquals("m", m1.getUnit().toString());
+	@Test
+	public void testMicro2() {
+		Quantity<Length> m1 = Quantities.getQuantity(1.0, Units.METRE);
+		assertEquals(1d, m1.getValue());
+		assertEquals("m", m1.getUnit().toString());
 
-    Quantity<Length> m2 = m1.to(MICRO(Units.METRE));
-    assertNumberEquals(1000_000L, m2.getValue(), 1E-12);
-    assertEquals("μm", m2.getUnit().toString());
-  }
+		Quantity<Length> m2 = m1.to(MICRO(Units.METRE));
+		assertNumberEquals(1000_000L, m2.getValue(), 1E-12);
+		assertEquals("μm", m2.getUnit().toString());
+	}
 
-  @Test
-  public void testNano() {
-    Quantity<Mass> m1 = Quantities.getQuantity(1.0, Units.GRAM);
-    assertEquals(1d, m1.getValue());
-    assertEquals("g", m1.getUnit().toString());
+	@Test
+	public void testNano() {
+		Quantity<Mass> m1 = Quantities.getQuantity(1.0, Units.GRAM);
+		assertEquals(1d, m1.getValue());
+		assertEquals("g", m1.getUnit().toString());
 
-    Quantity<Mass> m2 = m1.to(NANO(Units.GRAM));
-    assertNumberEquals(1000_000_000L, m2.getValue(), 1E-12);
-    assertEquals("ng", m2.getUnit().toString());
-  }
+		Quantity<Mass> m2 = m1.to(NANO(Units.GRAM));
+		assertNumberEquals(1000_000_000L, m2.getValue(), 1E-12);
+		assertEquals("ng", m2.getUnit().toString());
+	}
 
-  @Test
-  public void testNano2() {
-    Quantity<Length> m1 = Quantities.getQuantity(1.0, Units.METRE);
-    assertEquals(1d, m1.getValue());
-    assertEquals("m", m1.getUnit().toString());
+	@Test
+	public void testNano2() {
+		Quantity<Length> m1 = Quantities.getQuantity(1.0, Units.METRE);
+		assertEquals(1d, m1.getValue());
+		assertEquals("m", m1.getUnit().toString());
 
-    Quantity<Length> m2 = m1.to(NANO(Units.METRE));
-    assertNumberEquals(1000000000L, m2.getValue(), 1E-12);
-    assertEquals("nm", m2.getUnit().toString());
-  }
+		Quantity<Length> m2 = m1.to(NANO(Units.METRE));
+		assertNumberEquals(1000000000L, m2.getValue(), 1E-12);
+		assertEquals("nm", m2.getUnit().toString());
+	}
 
-  @Test
-  public void testHashMapAccessingMap() {
-    assertThat(LITRE.toString(), is("l"));
-    assertThat(MILLI(LITRE).toString(), is("ml"));
-    assertThat(MILLI(GRAM).toString(), is("mg"));
-  }
+	@Test
+	public void testHashMapAccessingMap() {
+		assertThat(LITRE.toString(), is("l"));
+		assertThat(MILLI(LITRE).toString(), is("ml"));
+		assertThat(MILLI(GRAM).toString(), is("mg"));
+	}
 
-  @Test
-  @Disabled("both Units have different UnitConverters, check for equality will always fail")
-  public void testSingleOperation() {
-    assertEquals(MICRO(GRAM), GRAM.divide(1_000_000));
-  }
-  
-  @SuppressWarnings({ "unchecked", "rawtypes" })
-@Test
-  public void testEquivalence() {
-	final ComparableUnit a = (ComparableUnit) MICRO(GRAM); 
-	final ComparableUnit b = (ComparableUnit) GRAM.divide(1_000_000);
-	  assertEquals(true, a.isEquivalentOf(b));
-	  assertEquals(true, b.isEquivalentOf(a));
-  }
+	@Test
+	@Disabled("both Units have different UnitConverters, check for equality will always fail")
+	public void testSingleOperation() {
+		assertEquals(MICRO(GRAM), GRAM.divide(1_000_000));
+	}
 
-  @Test
-  @Disabled("This is research for https://github.com/unitsofmeasurement/uom-se/issues/164")
-  public void testNestedOperationsShouldBeSame() {
-    Unit<Mass> m1 = MICRO(GRAM);
-    Unit<Mass> m2 = GRAM.divide(1000).divide(1000);
-    UnitConverter c1 = m1.getConverterTo(m2);
-    UnitConverter c2 = m2.getConverterTo(m1);
-    assertEquals(c1, c2);
-    assertEquals(m1, m2);
-  }
-  
-  @Test
-  public void testNestedEquivalence() {
-	  ComparableUnit<Mass> a = (ComparableUnit<Mass>) MICRO(GRAM);
-	  ComparableUnit<Mass> b = (ComparableUnit<Mass>) GRAM.divide(1000).divide(1000);
-	  assertEquals(true, a.isEquivalentOf(b));
-	  assertEquals(true, b.isEquivalentOf(a));
-  }
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Test
+	public void testEquivalence() {
+		final ComparableUnit a = (ComparableUnit) MICRO(GRAM);
+		final ComparableUnit b = (ComparableUnit) GRAM.divide(1_000_000);
+		assertEquals(true, a.isEquivalentOf(b));
+		assertEquals(true, b.isEquivalentOf(a));
+	}
 
-  @Test
-  public void testNestedOperationsNotTheSame() {
-    Unit<Mass> m1 = MICRO(GRAM);
-    Unit<Mass> m2 = GRAM.divide(1000).divide(2000);
-    UnitConverter c1 = m1.getConverterTo(m2);
-    UnitConverter c2 = m2.getConverterTo(m1);
-    assertNotEquals(c1, c2);
-    assertNotEquals(m1, m2);
-  }
-  
-  @Test
-  public void testKibi() {
-    final RationalNumber rational = RationalNumber.of(128, 125);
-    final UnitConverter converter = KIBI(METRE).getConverterTo(KILO(METRE));
-    assertEquals("Ki", KIBI.getSymbol());
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
+	@Test
+	@Disabled("This is research for https://github.com/unitsofmeasurement/uom-se/issues/164")
+	public void testNestedOperationsShouldBeSame() {
+		Unit<Mass> m1 = MICRO(GRAM);
+		Unit<Mass> m2 = GRAM.divide(1000).divide(1000);
+		UnitConverter c1 = m1.getConverterTo(m2);
+		UnitConverter c2 = m2.getConverterTo(m1);
+		assertEquals(c1, c2);
+		assertEquals(m1, m2);
+	}
 
-  @Test
-  public void testMebi() {
-	final RationalNumber rational = RationalNumber.of(16384, 15625);
-    final UnitConverter converter = MEBI(METRE).getConverterTo(MEGA(METRE));
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
+	@Test
+	public void testNestedEquivalence() {
+		ComparableUnit<Mass> a = (ComparableUnit<Mass>) MICRO(GRAM);
+		ComparableUnit<Mass> b = (ComparableUnit<Mass>) GRAM.divide(1000).divide(1000);
+		assertEquals(true, a.isEquivalentOf(b));
+		assertEquals(true, b.isEquivalentOf(a));
+	}
 
-  @Test
-  public void testGibi() {
-	final BigInteger exactFactor = BigInteger.valueOf(GIBI.getBase()).pow(GIBI.getExponent());
-	assertEquals(1073741824L, exactFactor.longValue());
-	
-    final RationalNumber rational = RationalNumber.of(2_097_152L, 1_953_125L);
-    final UnitConverter converter = GIBI(METRE).getConverterTo(GIGA(METRE));
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
+	@Test
+	public void testNestedOperationsNotTheSame() {
+		Unit<Mass> m1 = MICRO(GRAM);
+		Unit<Mass> m2 = GRAM.divide(1000).divide(2000);
+		UnitConverter c1 = m1.getConverterTo(m2);
+		UnitConverter c2 = m2.getConverterTo(m1);
+		assertNotEquals(c1, c2);
+		assertNotEquals(m1, m2);
+	}
 
-  @Test
-  public void testTebi() {
-	final RationalNumber rational = RationalNumber.of(268_435_456L, 244_140_625L);
-    final UnitConverter converter = TEBI(LITRE).getConverterTo(TERA(LITRE));
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
+	@Test
+	public void testKibi() {
+		final RationalNumber rational = RationalNumber.of(128, 125);
+		final UnitConverter converter = KIBI(METRE).getConverterTo(KILO(METRE));
+		assertEquals("Ki", KIBI.getSymbol());
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
 
-  @Test
-  public void testPebi() {
-	final RationalNumber rational = RationalNumber.of(34_359_738_368L, 30_517_578_125L);
-    final UnitConverter converter = PEBI(LITRE).getConverterTo(PETA(LITRE));
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
-  
-  @Test
-  public void testExbi() {
-	final RationalNumber rational = RationalNumber.of(4_398_046_511_104L, 3_814_697_265_625L);
-    final UnitConverter converter = EXBI(GRAM).getConverterTo(EXA(GRAM));
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
-  
-  @Test
-  public void testZebi() {
-	final RationalNumber rational = RationalNumber.of(562_949_953_421_312L, 47_683_715_8203_125L);
-    final UnitConverter converter = ZEBI(GRAM).getConverterTo(ZETTA(GRAM));
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
-  
-  @Test
-  public void testYobi() {
-	final RationalNumber rational = RationalNumber.of(72_057_594_037_927_936L, 59_604_644_775_390_625L); 
-    final UnitConverter converter = YOBI(GRAM).getConverterTo(YOTTA(GRAM));
-    assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
-    assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
-  }
-  
-  
-  // -- HELPER
+	@Test
+	public void testMebi() {
+		final RationalNumber rational = RationalNumber.of(16384, 15625);
+		final UnitConverter converter = MEBI(METRE).getConverterTo(MEGA(METRE));
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
 
-  
-  @Test
-  public void testPrefixMethod() {
-    assertEquals(CENTI(METRE), METRE.prefix(CENTI));
-  }
+	@Test
+	public void testGibi() {
+		final BigInteger exactFactor = BigInteger.valueOf(
+				GIBI.getValue().longValue()).pow(GIBI.getExponent());
+		assertEquals(1073741824L, exactFactor.longValue());
+
+		final RationalNumber rational = RationalNumber.of(2_097_152L, 1_953_125L);
+		final UnitConverter converter = GIBI(METRE).getConverterTo(GIGA(METRE));
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
+
+	@Test
+	public void testTebi() {
+		final RationalNumber rational = RationalNumber.of(268_435_456L, 244_140_625L);
+		final UnitConverter converter = TEBI(LITRE).getConverterTo(TERA(LITRE));
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
+
+	@Test
+	public void testPebi() {
+		final RationalNumber rational = RationalNumber.of(34_359_738_368L, 30_517_578_125L);
+		final UnitConverter converter = PEBI(LITRE).getConverterTo(PETA(LITRE));
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
+
+	@Test
+	public void testExbi() {
+		final RationalNumber rational = RationalNumber.of(4_398_046_511_104L, 3_814_697_265_625L);
+		final UnitConverter converter = EXBI(GRAM).getConverterTo(EXA(GRAM));
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
+
+	@Test
+	public void testZebi() {
+		final RationalNumber rational = RationalNumber.of(562_949_953_421_312L, 47_683_715_8203_125L);
+		final UnitConverter converter = ZEBI(GRAM).getConverterTo(ZETTA(GRAM));
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
+
+	@Test
+	public void testYobi() {
+		final RationalNumber rational = RationalNumber.of(72_057_594_037_927_936L, 59_604_644_775_390_625L);
+		final UnitConverter converter = YOBI(GRAM).getConverterTo(YOTTA(GRAM));
+		assertNumberEquals(rational.getDividend(), converter.convert(rational.getDivisor()), 1E-12);
+		assertNumberEquals(rational.doubleValue(), converter.convert(1.), 1E-12);
+	}
+
+	// -- HELPER
+
+	@Test
+	public void testPrefixMethod() {
+		assertEquals(CENTI(METRE), METRE.prefix(CENTI));
+	}
 }
