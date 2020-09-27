48b46c25a79321c22c23dfa16eb2c8f2b908fb28
Werner Keil <werner.keil@gmx.net>
2020-09-27 02:44:01 +0200

    278: EBNFUnitFormat doesn't always include the prefix
    Task-Url: https://github.com/unitsofmeasurement/indriya/issues/278

--- src/test/java/tech/units/indriya/spi/ServiceTest.java
+++ src/test/java/tech/units/indriya/spi/ServiceTest.java
@@ -29,7 +29,11 @@
  */
 package tech.units.indriya.spi;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
