7d280f9b4dbeae407477d39451dd4f068041364e
Andi Huber <ahuber@apache.org>
2020-10-01 10:08:29 +0200

    309: add SerializationRoundTrip Test for ProductUnits

---
+++ src/test/java/tech/units/indriya/SerializationRoundTrip.java
@@ -0,0 +1,76 @@
+/*
+ * Units of Measurement Reference Implementation
+ * Copyright (c) 2005-2020, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-385, Indriya nor the names of their contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tech.units.indriya;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.jupiter.api.Assertions;
+
+/**
+ *
+ * @author Andi Huber
+ */
+public final class SerializationRoundTrip {
+
+    /**
+     * Verify that after a serializing de-serializing cycle given {@code value} is equal to 
+     * the original value.
+     * @param value
+     */
+    public static void assertProperSerializationRoundTrip(Object value) {
+        try {
+            Assertions.assertEquals(value, serializationRoundTrip(value));
+        } catch (Exception e) {
+            Assertions.fail(e);
+        }
+    }
+    
+    // -- HELPER
+    
+    @SuppressWarnings("unchecked")
+    private static <T> T serializationRoundTrip(final T value) throws Exception {
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(out)) {
+                oos.writeObject(value);
+                oos.flush();
+            }
+            try (ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray())) {
+                try (ObjectInputStream ois = new ObjectInputStream(in)) {
+                    return (T) ois.readObject();
+                }
+            }
+        }
+    }
+    
+
+}
