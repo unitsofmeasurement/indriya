7fbdaa45463702c0bc1532d92addf6623fbe6d01
Werner Keil <werner.keil@gmx.net>
2022-12-15 01:11:14 +0100

    Update sh

--- src/main/config/countfiles.sh
+++ src/main/config/countfiles.sh
@@ -1,1 +1,4 @@
-find ~/repo/./target/surefire-reports -iname "*.txt" -exec grep -Li "Failures: 0" {} \+
+#!/bin/sh
+# find ~/repo/./target/surefire-reports -iname "*.txt" -exec grep -Li "Failures: 0" {} \+
+number_of_failures=`grep -L -r -i "Failures: 0" ~/repo/./target/surefire-reports/*.txt | wc -l`
+echo $number_of_failures
