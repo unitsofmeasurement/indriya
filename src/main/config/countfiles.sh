69b9b65f55837c2494170df6cb171947bfa22183
Werner Keil <werner.keil@gmx.net>
2022-12-15 01:23:01 +0100

    Updated sh

--- src/main/config/countfiles.sh
+++ src/main/config/countfiles.sh
@@ -3,5 +3,5 @@
 number_of_failures=`grep -L -r -i "Failures: 0" ~/repo/./target/surefire-reports/*.txt | wc -l`
 if (( $number_of_failures != 0 )); then
 	echo $number_of_failures
-	exit 1
+	return 1
 fi
