c2738fe6d6a9eb6946931c99fa845b569a2fde2a
Werner Keil <werner.keil@gmx.net>
2022-12-15 21:18:06 +0100

    Update CI settings

--- src/main/config/checktests.sh
+++ src/main/config/checktests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-find ~/repo/./target/surefire-reports -iname "*.txt" -exec grep -Li "Failures: 0" {} \+
-number_of_failures=`grep -L -r -i "Failures: 0" ~/repo/./target/surefire-reports/*.txt | wc -l`
-#if [ "$number_of_failures" -ne "0" ]; then
-#	echo $number_of_failures files with failures.
-#	exit 2
-#fi
-number_of_errors=`grep -L -r -i "Errors: 0" ~/repo/./target/surefire-reports/*.txt | wc -l`
+# find ~/repo/./target/surefire-reports -iname "*.txt" -exec grep -Li "Failures: 0" {} \+
+number_of_failures=`grep -L -r -i "Failures: 0" ~/repo/./target/surefire-reports/*Test.txt | wc -l`
+if [ "$number_of_failures" -ne "0" ]; then
+	echo $number_of_failures files with failures.
+	exit 2
+fi
+number_of_errors=`grep -L -r -i "Errors: 0" ~/repo/./target/surefire-reports/*Test.txt | wc -l`
 if [ "$number_of_errors" -ne "0" ]; then
 	echo $number_of_errors files with errors.
 	exit 2
