0bd12b77a6a840a41e96a28d92c2eb91eefca540
Werner Keil <werner.keil@gmx.net>
2022-12-15 01:27:20 +0100

    other form of if

--- src/main/config/countfiles.sh
+++ src/main/config/countfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # find ~/repo/./target/surefire-reports -iname "*.txt" -exec grep -Li "Failures: 0" {} \+
 number_of_failures=`grep -L -r -i "Failures: 0" ~/repo/./target/surefire-reports/*.txt | wc -l`
-if (( $number_of_failures != 0 )); then
+if [ "$number_of_failures" -ne "0" ]; then
 	echo $number_of_failures
-	return 1
+	exit 2
 fi
